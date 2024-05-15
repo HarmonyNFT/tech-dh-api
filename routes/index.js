de44bbb51195b0f0375cd98b86a7b5ded3db6557
Harmony <d.haussmann@blockchain-atlas.de>
2024-05-15 15:12:25 +0200

    added pokemon api

--- routes/index.js
+++ routes/index.js
@@ -24,14 +24,34 @@ router.get('/weather/:city', async (req, res) => {
             temperature: data.current.temp_c,
             condition: data.current.condition.text,
             icon: data.current.condition.icon,
-        //     temperature: (data.main.temp - 273.15).toFixed(2), // Kelvin in Celsius umrechnen
-        //     description: data.weather[0].description,
-        //     humidity: data.main.humidity,
-        //     windSpeed: data.wind.speed
         };
         res.json(weatherData);
     } catch (error) {
         res.status(500).json({ error: 'Fehler beim Abrufen der Wetterdaten' });
+    }
+});
+
+router.get('/pokemon/:pokemon', async (req, res) => {
+
+    const pokemon = req.params.pokemon;
+    
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+
+       try {
+        const response = await axios.get(url);
+        const data = response.data;
+
+
+        
+        const pokemonData = {
+            name: data.name,
+            number: data.id,
+            icon: data.sprites.front_default,
+       
+        };
+        res.json(pokemonData);
+    } catch (error) {
+        res.status(500).json({ error: 'Fehler beim Abrufen der Pokemondaten' });
     }
 });
 
